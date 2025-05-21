3d1e3bb50f018f7d980274602b5cc88513f4c0ca
zaydolas74 <r0934607@student.thomasmore.be>
2025-05-22 01:32:36 +0200

    Mapindeling Update/Fix

--- ReactFightZone/src/pages/Dashboard/index.js
+++
@@ -1,4 +0,0 @@
-export { default as TrainerDashboard } from "./TrainerDashboard";
-export { default as VechterDashboard } from "./VechterDashboard";
-export { default as VkbmoDashboard } from "./VkbmoDashboard";
-import "../../assets/styles/pages/Dashboard.module.css";
