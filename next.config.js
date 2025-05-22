bd2f294c76be92f68eb3390c7025e1e5c3f14031
zaydolas74 <r0934607@student.thomasmore.be>
2025-05-22 04:50:06 +0200

    Vervel Font Fix

--- next.config.js
+++ next.config.js
@@ -17,6 +17,23 @@ const nextConfig = {
     domains: ["fightzone-api.onrender.com"],
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/fonts/:font*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
